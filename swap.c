a5c8a335fdd14340cb1ece5fb10e1468bd202e5b
bckarthik7 <44445298+bckarthik7@users.noreply.github.com>
2021-07-01 19:18:57 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,16 @@
+void swap_max(int arr[], int l, int n){
+  int b,c, large = INT_MIN;
+  for (int i=n;i<l;i++)
+    if(arr[i]>large)
+      large = arr[i];
+  for (int i=n;i<l;i++)
+    if(large==arr[i])
+      c=i;
+  b=arr[n];
+  arr[n]=arr[c];
+  arr[c]=b;
+}
+void ssort(int arr[], int l){
+  for(int i=0;i<l;i++)
+    swap_max(arr,l,i);
+}
